c9aaef155e0b4de6e21c6c9870dbfcbd1485a0f5
Heather Nye <heatherdnye@gmail.com>
2020-10-07 17:51:33 -0500

    holy sh*t i got expensive to work

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,19 @@
 # Your code here
 
-
+cache = {}
 def expensive_seq(x, y, z):
     # Your code here
-    
+    if x <= 0:
+        return y + z
+    if x in cache:
+        return cache[x]
+    if x > 0:
+        first_return = expensive_seq(x-1,y+1,z)
+        second_return = expensive_seq(x-2,y+2,z*2)
+        third_return = expensive_seq(x-3,y+3,z*3)        
+    results = tuple([first_return, second_return, third_return])    
+    cache[x] = results
+    return results
 
 
 if __name__ == "__main__":
@@ -12,3 +22,4 @@ if __name__ == "__main__":
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
+
