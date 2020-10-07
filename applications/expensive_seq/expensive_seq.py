b93a01df6c86d43d7c6c47897d55bf585712b0cb
Heather Nye <heatherdnye@gmail.com>
2020-10-07 18:44:59 -0500

    started count

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -5,13 +5,6 @@ def expensive_seq(x, y, z):
     # Your code here
     if x <= 0:
         return y + z    
-    # if x > 0:
-    #     first_return = expensive_seq(x-1,y+1,z)
-    #     second_return = expensive_seq(x-2,y+2,z*2)
-    #     third_return = expensive_seq(x-3,y+3,z*3)        
-    # results = tuple((first_return, second_return, third_return))    
-    # cache[x] = results
-    # return results
     key = (x, y, z)
     if key not in cache:           
         results = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
