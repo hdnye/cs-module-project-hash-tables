baa89bfc99283a4e03ddbf916bfc789c3f349c64
Heather Nye <heatherdnye@gmail.com>
2020-10-08 13:48:20 -0500

    words only returning 1st word of string

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,79 @@
 
 # Your code here
 
+# first pass: find the char's with the max & min values
+# create dict
+# find the max diff btn any two char's & assign to each other
+# add to dict
+
+
+
+
+
+
+
+
+
+# implement 2 f()s Encrypt & Decrypt
+# Build a table to hold the char's & assign their new value
+encode_table = {
+    'A' : 'H',
+    'B' : 'Z',
+    'C' : 'Y',
+    'D' : 'W',
+    'E' : 'O',
+    'F' : 'R',
+    'G' : 'J',
+    'H' : 'D',
+    'I' : 'P',
+    'J' : 'T',
+    'K' : 'I',
+    'L' : 'G',
+    'M' : 'L',
+    'N' : 'C',
+    'O' : 'E',
+    'P' : 'X',
+    'Q' : 'K',
+    'R' : 'U',
+    'S' : 'N',
+    'T' : 'F',
+    'U' : 'A',
+    'V' : 'M',
+    'W' : 'B',
+    'X' : 'Q',
+    'Y' : 'V',
+    'Z' : 'S',
+    ' ' : ' '
+}
+decode_table = {}
+
+# will use the encode table to build the decode table
+def build_decode_table(encode_table):
+    for k, v in encode_table.items():
+        decode_table[v] = k
+    return decode_table
+
+build_decode_table(encode_table)
+print(build_decode_table(encode_table))
+
+# Encrypt will take a nomral Eng str & jumble it using a Caesar Cypher
+# (substitution cypher)
+def encrypt(string): 
+    encrypted_string = ''
+    # loop through string
+    for char in string:
+        encrypted_string += encode_table[char]
+    return encrypted_string
+
+print(encrypt('HELLO WORLD'))
+
+# Decrypt reverses the process
+def decrypt(string):
+    encrypted_string = ''
+    # loop through string
+    for char in string:
+        encrypted_string += decode_table[char]
+    return encrypted_string
+
+encrypted_message = encrypt("HELLO WORLD")
+print(decrypt(encrypted_message))
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,58 @@
 def word_count(s):
     # Your code here
-
-
+    d = {}
+  
+    for words in s.split():
+        words = words.strip(r": , . - + = / \ | [] {}() * ^ & ")
+        words = words.lower()
+        if words in d:
+            d[words] += 1
+        else:
+            d[words] = 1  
+        return d        
+   
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
+
+
+# letter_count example
+# def letter_count(string):
+#     # create an empty list
+#     d = {}
+#     # loop through string
+#     for char in string: 
+#         # is it in the string? increment its value
+#         if char in d:
+#             d[char] += 1
+#         else:
+#         # if it does not exist, add it to dict
+#             d[char] = 1
+#     return d  
+
+# def print_sorted_letter_count(string):
+#     # get the sorted dict
+#     d = letter_count(string)    
+#     # convert dict to a list
+#     # built-in items()
+#     items = list(d.items())
+#     print(items)
+#     # print the items out in alph order
+#     # built-in function similar to ()=> in JS
+#     # ex: items.map((item) => { console.log(item) })
+#     items.sort(key=lambda item: item[0].lower(), reverse=False)
+#     print(items)
+#     # this will print the most used char's
+#     for i in items:
+#         print(f'{i[0]}: {i[1]}')
+
+# if __name__ == "__main__":
+#     print(letter_count(""))
+#     print(letter_count("Hello"))
+#     print(letter_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+#     print(print_sorted_letter_count(
+#         'This is a test of the emergency broadcast network. This is only a test.'))
